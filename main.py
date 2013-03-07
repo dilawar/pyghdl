8c2fe84291fb84494724622d16e99e24b69f3ad0
Dilawar Singh <dilawar@ee.iitb.ac.in>
2013-03-07 17:06:42 +0000

    Filter is working. We can pass the regex to get the files we want.

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 #!/usr/bin/env python 
 import curses
 import argparse
+import os
+
+def findListings(dirs, regex=None) :
+  listings = set()
+  for dir in dirs :
+    dir = dir.pop()
+    for dirPath, subdirPath, fileList in os.walk(dir) :
+      for fname in fileList :
+        fileFullPath = os.path.join(dir, fname)
+        listings.add(fileFullPath)
+  if not regex :
+    return listings
+  else :
+    import re
+    return set(filter(lambda x : re.match(regex, x), listings))
 
 
-# Argument parser.
-description = '''Python front-end of ghdl written for personal convenience.'''
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument('-d', metavar='design_directory', nargs=1
-    , action='append'
-    , required = True
-    , help = 'Location of directory where your VHDL design.')
-parser.add_argument('-t', metavar='top_module', nargs=1
-    , dest = 'topModule'
-    , required = False
-    , help = 'Optional : Top module in your design.'
-    )
+if __name__=="__main__" :
+  # Argument parser.
+  description = '''Python front-end of ghdl written for personal convenience.'''
+  parser = argparse.ArgumentParser(description=description)
+  parser.add_argument('-d', metavar='design_directory', nargs=1
+      , action='append'
+      , required = True
+      , help = 'Location of directory where your VHDL design.')
+  parser.add_argument('-t', metavar='top_module', nargs=1
+      , dest = 'topModule'
+      , required = False
+      , help = 'Optional : Top module in your design.'
+      )
 
-args = parser.parse_args()
+  class Args:
+    pass 
+  args = Args()
+  parser.parse_args(namespace=args)
+  files = findListings(args.d, regex=".*py$")
+  print(files)
 
