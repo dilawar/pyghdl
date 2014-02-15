0ec49bb175b882efde8c6ee42859a7a5c58084ab
Dilawar <dilawars@iitb.ac.in>
2014-02-15 14:55:17 +0530

    Renamed to fontend

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 rm -f ./test/memory_subsystem/strictly_ordered/auto_generated*
-./testbench/main.py -d  ./test/memory_subsystem/strictly_ordered -t orderded_memory_subsystem 
+./frontend/main.py -d  ./test/memory_subsystem/strictly_ordered -t orderded_memory_subsystem 
