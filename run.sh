35cfde8424e689200a64ca9bc015a0f9e2d0fe5d
Dilawar <dilawars@iitb.ac.in>
2014-02-15 14:32:48 +0530

    Okies. If top module is given then it works cool

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 rm -f ./test/memory_subsystem/unordered/auto_generated*
-./testbench/main.py -d  ./test/memory_subsystem/unordered/ -t CombinationalMux
+./testbench/main.py -d  ./test/memory_subsystem/strictly_ordered -t orderded_memory_subsystem 
